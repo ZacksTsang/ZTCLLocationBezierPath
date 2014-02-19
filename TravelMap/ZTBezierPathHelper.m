5cd0f1cb18621ea8f7f6a1de9b02dbd677719d86
Zacks Tsang <zshaojia@gmail.com>
2014-02-19 16:20:45 +0800

    modify helper

--- TravelMap/ZTBezierPathHelper.m
+++ TravelMap/ZTBezierPathHelper.m
@@ -13,6 +13,57 @@
 
 @implementation ZTBezierPathHelper
 
+
+
+/**
+ * p1 -> 起始点 CLLocationCoordinate2D
+ *
+ * p2 -> 结束点 CLLocationCoordinate2D
+ *
+ * isClockWise -> 是否顺时针方向 YES NO
+ */
++(NSMutableArray *)bezierPath:(CLLocationCoordinate2D)p1 targetPoint:(CLLocationCoordinate2D)p2 clockwise:(BOOL)isClockWise{
+    
+    
+    //get the auxiliary point
+    CLLocationCoordinate2D auxiliaryPoint = [self fetchThirdPointByLocations:p1 withEndLocation:p2 withAngle:ANGEL_OF_AUXILIARY_POINT clockwise:isClockWise];
+    
+    float bezier1x;
+    float bezier1y;
+    float bezier2x;
+    float bezier2y;
+    
+    NSMutableArray *targetPoints=[NSMutableArray arrayWithCapacity:3];
+    
+    float bezier_x,bezier_y;
+    
+    float t = 0;
+    //t between 0.01 and 1
+    while ( [targetPoints count]<=100 ) {
+        
+        //get the start point of a Bezier curve
+        bezier1x = p1.longitude + ( auxiliaryPoint.longitude - p1.longitude ) * t;
+        bezier1y = p1.latitude + ( auxiliaryPoint.latitude - p1.latitude ) * t;
+        
+        //get the end point of a Bezier curve
+        bezier2x = auxiliaryPoint.longitude + ( p2.longitude - auxiliaryPoint.longitude ) * t;
+        bezier2y = auxiliaryPoint.latitude + ( p2.latitude - auxiliaryPoint.latitude ) * t;
+        
+        //get the point of quadratic Bezier curve
+        bezier_x = bezier1x + ( bezier2x - bezier1x ) * t;
+        bezier_y  = bezier1y + ( bezier2y - bezier1y ) * t;
+        
+        CLLocation *bezierPoint=[[CLLocation alloc] initWithLatitude:bezier_y longitude:bezier_x];
+        [targetPoints addObject:bezierPoint];
+        
+        t += 0.01f;
+        
+    }
+    
+    return targetPoints;
+}
+
+
 +(CLLocationCoordinate2D)fetchThirdPointByLocations:(CLLocationCoordinate2D)startLoc withEndLocation:(CLLocationCoordinate2D)endLoc withAngle:(float)angle  clockwise:(BOOL)isClockWise{
     
     CLLocationCoordinate2D target;
@@ -72,46 +123,5 @@
     
 }
 
-
-+(NSMutableArray *)bezierPath:(CLLocationCoordinate2D)p1 targetPoint:(CLLocationCoordinate2D)p2 clockwise:(BOOL)isClockWise{
-    
-    
-    //get the auxiliary point 
-    CLLocationCoordinate2D auxiliaryPoint = [self fetchThirdPointByLocations:p1 withEndLocation:p2 withAngle:ANGEL_OF_AUXILIARY_POINT clockwise:isClockWise];
-
-    float bezier1x;
-    float bezier1y;
-    float bezier2x;
-    float bezier2y;
-    
-    NSMutableArray *targetPoints=[NSMutableArray arrayWithCapacity:3];
-
-    float bezier_x,bezier_y;
-    
-    float t = 0;
-    //t between 0.01 and 1
-    while ( [targetPoints count]<=100 ) {
- 
-        //get the start point of a Bezier curve
-        bezier1x = p1.longitude + ( auxiliaryPoint.longitude - p1.longitude ) * t;
-        bezier1y = p1.latitude + ( auxiliaryPoint.latitude - p1.latitude ) * t;
-        
-        //get the end point of a Bezier curve
-        bezier2x = auxiliaryPoint.longitude + ( p2.longitude - auxiliaryPoint.longitude ) * t;
-        bezier2y = auxiliaryPoint.latitude + ( p2.latitude - auxiliaryPoint.latitude ) * t;
-        
-        //get the point of quadratic Bezier curve
-        bezier_x = bezier1x + ( bezier2x - bezier1x ) * t;
-        bezier_y  = bezier1y + ( bezier2y - bezier1y ) * t;
-        
-        CLLocation *bezierPoint=[[CLLocation alloc] initWithLatitude:bezier_y longitude:bezier_x];
-        [targetPoints addObject:bezierPoint];
-        
-        t += 0.01f;
-        
-    }
-    
-    return targetPoints;
-}
 
 @end
